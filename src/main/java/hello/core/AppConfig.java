9c90d662988d43c48dd2691e229079d72f2199b5
yuju <yujjuu22@gmail.com>
2023-08-04 18:36:41 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -13,10 +15,19 @@ public class AppConfig {
     //이전에는 객체를 생성하고 어떤 인터페이스가 들어가야하는지 할당하는것을 MemberService안에서 직접함
     //생성자주입
     public MemberService memberService() { //MemberSerivice를 부르면
-        return new MemberServiceImpl(new MemoryMemberRepository()); //이때 memroyMemberRepository가 여기서 들어감
+//        return new MemberServiceImpl(new MemoryMemberRepository()); //이때 memroyMemberRepository가 여기서 들어감  //ctrl + alt + m
+        return new MemberServiceImpl(memberRepository()); //이때 memroyMemberRepository가 여기서 들어감  //ctrl + alt + m
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());//생성자 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy());//생성자 주입
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
