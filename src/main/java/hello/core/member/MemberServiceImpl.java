084cd6437d0998292a46c6c095510647f531f2ed
yuju <yujjuu22@gmail.com>
2023-08-09 14:44:05 +0900

    컴포넌트스캔 - 컴포넌트스캔과 의존관계 자동주입하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     //얘가 가입을 하고 회원을 찾으려면 뭐가 필요하지?
@@ -8,6 +12,8 @@ public class MemberServiceImpl implements MemberService{
 //    private final MemberRepository memberRepository = new MemoryMemberRepository(); //인터페이스가 필요(구현객체도 필요)
     private final MemberRepository memberRepository;  //MemberRepository 라는 인터페이스만 존재 <- 추상화에만 의존
 
+    //자동 의존관계주입
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
