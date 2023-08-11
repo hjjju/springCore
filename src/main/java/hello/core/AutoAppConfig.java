824d137755b51a7e00a893bdc3e6e3fed9990ab1
yuju <yujjuu22@gmail.com>
2023-08-11 20:23:35 +0900

    컴포넌트스캔- 필터, 중복등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,21 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-@Configuration
+@Configuration //지정하지않으면 Componentscan이 붙은 설정 정보 클래스의 패키지가 시작위치가 된다.
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)  //default:여기서부터 뒤짐 hello.core
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 
 }
